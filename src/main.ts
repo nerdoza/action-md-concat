5ccb1a2bcce8183c8172ed14f487c391973f1545
Zach Cardoza <2280384+bayssmekanique@users.noreply.github.com>
2022-02-23 15:17:53 -0800

    Pushes to v1.0.2 Fixes another bug in limit option

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import ConcatFiles from './concat'
 async function run (): Promise < void > {
   try {
     const srcDir = core.getInput('srcDir', { required: true })
-    const limit = core.getInput('limit') ?? undefined
+    const limit = core.getInput('limit') || undefined
     const destFile = core.getInput('destFile', { required: true })
-    const separator = core.getInput('separator') ?? '\n'
+    const separator = core.getInput('separator') || '\n'
 
     await ConcatFiles(srcDir, destFile, limit, separator)
 
